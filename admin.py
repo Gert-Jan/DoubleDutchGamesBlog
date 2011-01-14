3cec0b6f9464794cf09f20f71fe27882c29bb098
G.J. Stolk <Stolk.GJ@gmail.com>
2011-01-14 18:55:27 +0100

    Added a page system. The page system allows the creation of static pages outside of the blog. A completely customizable page can be used for this, so a page can be fully in the style of the blog or have it's own style. Changed the admin panel: - Removed the new post tab, replaced it with a link in the page. - Added a Pages tab.

--- admin.py
+++ admin.py
@@ -13,11 +13,17 @@ webapp.template.register_template_library('django_helper')
 
 application = webapp.WSGIApplication([
   (config.url_prefix + '/admin/', handlers.AdminHandler),
+  (config.url_prefix + '/admin/posts', handlers.AdminHandler),
+  (config.url_prefix + '/admin/pages', handlers.PageAdminHandler),
   (config.url_prefix + '/admin/newpost', handlers.PostHandler),
   (config.url_prefix + '/admin/post/(\d+)', handlers.PostHandler),
   (config.url_prefix + '/admin/regenerate', handlers.RegenerateHandler),
   (config.url_prefix + '/admin/post/delete/(\d+)', handlers.DeleteHandler),
   (config.url_prefix + '/admin/post/preview/(\d+)', handlers.PreviewHandler),
+  (config.url_prefix + '/admin/newpage', handlers.PageHandler),
+  (config.url_prefix + '/admin/page/delete/(/.*)', handlers.PageDeleteHandler),
+  (config.url_prefix + '/admin/page/(/.*)', handlers.PageHandler),
+  (config.url_prefix + '/admin/regeneratepages', handlers.PageRegenerateHandler),
 ])
 
 
